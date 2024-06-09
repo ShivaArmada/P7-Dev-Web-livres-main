c0c1e5e64dca2ddccb48bb34335e916bcf2fde9b
ShivaArmada <planckaertg@gmail.com>
2024-06-09 13:52:02 +0200

    user quasi bon, logique implémentée dans controllers

--- Backend/controllers/User.js
+++ Backend/controllers/User.js
@@ -2,16 +2,61 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const saltRounds = 10;
 
+const generateToken = (userId) => {
+    return jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: "12h",
+    });
+}
 
+const handleError = (res, errorMessage, consoleMessage, statusCode = 500) => {
+    console.error(consoleMessage);
+    return res.status(statusCode).json({ error: errorMessage });
+}
 
-exports.signup = (req, res, next) => {
+exports.signUp = async (req, res, next) => {
+    const { email, motDePasse } = req.body;
 
+    try {
+        const hash = await bcrypt.hash(motDePasse, saltRounds);
+        const newUser = new User({ email, motDePasse: hash });
 
+        const user = await newUser.save();
+        const token = generateToken(user._id); // Passer l'ID de l'utilisateur à generateToken
+
+        res.status(201).json({
+            message: "Bienvenue sur MonVieuxGrimoire! Votre inscription a été réussie.",
+            token,
+        });
+    } catch (err) {
+        return handleError(res, 'Erreur lors de l\'enregistrement de l\'utilisateur', 'Erreur lors de l\'enregistrement de l\'utilisateur :', err);
+    }
 };
 
-exports.signin = (req, res, next) => {
+exports.signIn = async (req, res, next) => {
+    const { email, motDePasse } = req.body;
 
+    try {
+        const user = await User.findOne({ email: email });
 
+        if (!user) {
+            return handleError(res, 'Utilisateur non trouvé', 'Utilisateur non trouvé', 404);
+        }
 
+        const isMatch = await bcrypt.compare(motDePasse, user.motDePasse);
+
+        if (isMatch) {
+            const token = generateToken(user._id);
+            res.status(200).json({
+                message: "Connexion réussie",
+                token,
+            });
+        } else {
+            res.status(401).json({ error: 'Email/Mot de passe incorrect' });
+        }
+    } catch (err) {
+        return handleError(res, 'Erreur lors de la recherche de l\'utilisateur', 'Erreur lors de la recherche de l\'utilisateur :', err);
+    }
 };
+
--- Backend/routes/sign_up.js
+++ Backend/routes/sign_up.js
@@ -1,73 +1,12 @@
 /*eslint-disable*/
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/UserUp');
 const API_ROUTES = require('../../src/utils/constants'); // Assurez-vous que le chemin vers apiRoutes.js est correct
-const saltRounds = 10;
 
-userCtrl = require('../controllers/User');
+const userCtrl = require('../controllers/User');
 
-const generateToken = (userId) => {
-    return jwt.sign({ userId }, process.env.JWT_SECRET, {
-        expiresIn: "12h",
-    });
-}
+router.post(API_ROUTES.SIGN_UP, userCtrl.signUp);
+router.post(API_ROUTES.SIGN_IN, userCtrl.signIn);
 
-const handleError = (res, errorMessage, consoleMessage, statusCode = 500) => {
-    console.error(consoleMessage);
-    return res.status(statusCode).json({ error: errorMessage });
-}
-
-router.post(API_ROUTES.SIGN_UP, async (req, res) => {
-    const { email, motDePasse } = req.body;
-
-    try {
-        const hash = await bcrypt.hash(motDePasse, saltRounds);
-        const newUser = new User({ email, motDePasse: hash });
-
-        const user = await newUser.save();
-        const token = generateToken(user._id); // Passer l'ID de l'utilisateur à generateToken
-
-        res.status(201).json({
-            message: "Bienvenue sur MonVieuxGrimoire! Votre inscription a été réussie.",
-            token,
-        });
-    } catch (err) {
-        return handleError(res, 'Erreur lors de l\'enregistrement de l\'utilisateur', 'Erreur lors de l\'enregistrement de l\'utilisateur :', err);
-    }
-});
-
-router.post(API_ROUTES.SIGN_IN, async (req, res) => {
-    const { email, motDePasse } = req.body;
-
-    try {
-        const user = await User.findOne({ email: email });
-
-        if (!user) {
-            return handleError(res, 'Utilisateur non trouvé', 'Utilisateur non trouvé', 404);
-        }
-
-        const isMatch = await bcrypt.compare(motDePasse, user.motDePasse);
-
-        if (isMatch) {
-            const token = generateToken(user._id);
-            res.status(200).json({
-                message: "Connexion réussie",
-                token,
-            });
-        } else {
-            res.status(401).json({ error: 'Email/Mot de passe incorrect' });
-        }
-    } catch (err) {
-        return handleError(res, 'Erreur lors de la recherche de l\'utilisateur', 'Erreur lors de la recherche de l\'utilisateur :', err);
-    }
-});
-
-
-//une fois tout ranger dans controllers en deux fonctions, ça donne
-//router.post(API_ROUTES.SIGN_UP, userCtrl.signUp);
-//router.post(API_ROUTES.SIGN_IN, userCtrl.signIn);
 
 module.exports = router;
